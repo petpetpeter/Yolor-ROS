4dfff3d6257e197dc95d07828dc8fdd58ae10af7
petpetpeter <55285546+petpetpeter@users.noreply.github.com>
2022-03-30 14:55:03 +0900

    edit package installer

---
+++ scripts/yolor/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="yolor",
+    version='0.0.1',
+    install_requires=[
+                      'numpy',
+                      'matplotlib',
+                      "rospkg",
+                      "scipy",
+                      ]
+)
